d8c1621013721eea1d10ac169895db6c2fa9579a
anthony <anthony@anthony-VirtualBox.(none)>
2014-09-30 18:16:26 +0200

    casi listo

---
+++ src/OperacionesGeometricas.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// http://www.sc.ehu.es/sbweb/fisica/cursoJava/fundamentos/clase1/string.html
+
+	public class OperacionesGeometricas {
+
+	public static void main (String args[]) {
+	
+	string str1="OperacionesGeometricas";
+	String str2=new String ("Ciculo");
+	if(str1==str2){	
+	System.out.println("\nCirculo");
+	
+	}
+else
+	{ 
+	systen.out.println("circulo");
+	}
+
+	string str1="OperacionesGeometricas";
+	String str2=new String ("Rectangulo");
+	if(str1==str2){	
+	System.out.println("\nRectangulo");
+		
+	}
+else
+	 { 
+	systen.out.println("rectangulo");
+	}
+}
+}
